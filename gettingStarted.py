87fec1bd110bfae29a707b1fb19f33799ac0c79c
Raj Nair <256466382+rn-nyu@users.noreply.github.com>
2026-01-28 14:16:39 -0600

    Updated filename with answers for Q8, Q9 based on OSI model. Removed trailing trim functionality from user submitted question, so comparison should work, and updated SHA256 hashtag value

--- gettingStarted.py
+++ gettingStarted.py
@@ -32,7 +32,7 @@ def welcome_assignment_answers(question):
     elif clear_str(question) == "Is MD5 a secured hashing algorithm? - Yes/No".lower():
         answer = "No"
     elif clear_str(question) == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number".lower():
-        answer = 7
+        answer = 5
     elif clear_str(question) == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number".lower():
         answer = 3
     else:
